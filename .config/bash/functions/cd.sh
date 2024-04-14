b7bd2cf2f9f58d1b7596ae7833aab9c6234b9548
TarekSaeed0 <naminode2020@gmail.com>
2024-04-14 16:05:29 +0200

    .

--- .config/bash/functions/cd.sh
+++ .config/bash/functions/cd.sh
@@ -1,53 +1,38 @@
 #!/bin/bash
 
 cd() {
-	if [[ "$1" == "--" ]]; then
+	if [ "$1" = "--" ]; then
 		dirs -v
 		return 0
 	fi
 
 	local destination="$1"
-	if [[ -z "$destination" ]]; then
-		destination="$HOME"
-	fi
+	[ -z "$destination" ] && destination="$HOME"
 
-	if [[ "${destination:0:1}" == "-" ]]; then
+	if [ "${destination:0:1}" = "-" ]; then
 		local index="${destination:1}"
-		if [[ -z "$index" ]]; then
-			index=1
-		fi
+		[ -z "$index" ] && index=1
 
-		if ! destination="$(dirs +"$index" 2>/dev/null)"; then
-			return 1
-		fi
-	fi
-
-	if [[ "${destination:0:1}" == "~" ]]; then
+		destination="$(dirs -l +"$index" 2>/dev/null)" || return
+	elif [ "${destination:0:1}" = "~" ]; then
 		destination="$HOME${destination:1}"
 	fi
 
-	local index=0
+	pushd "$destination" &>/dev/null || return
+
+	destination="$PWD"
+
+	local index=1
 	while :; do
 		local directory
-		if ! directory="$(dirs +$index 2>/dev/null)"; then
-			break
-		fi
-		if [[ "${directory:0:1}" == "~" ]]; then
-			directory="$HOME${directory:1}"
-		fi
+		directory="$(dirs -l +$index 2>/dev/null)" || break
 
-		if [[ "$directory" -ef "$destination" ]]; then
-			if ! popd -n +$index &>/dev/null; then
-				break
-			fi
+		if [ "$directory" -ef "$destination" ]; then
+			popd -n +$index &>/dev/null || break
 		else
 			((index++))
 		fi
 	done
 
-	if ! pushd "$destination" &>/dev/null; then
-		return 1
-	fi
-
-	return 0
+	ls
 }
