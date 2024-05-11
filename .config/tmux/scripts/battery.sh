2124f2fe7c2ef2c2962e12d24c579c799627a1e6
TarekSaeed0 <naminode2020@gmail.com>
2024-05-11 19:12:22 +0300

    .

--- .config/tmux/scripts/battery.sh
+++ .config/tmux/scripts/battery.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-if command -v termux-battery-status &> /dev/null; then
+if command -v termux-battery-status &>/dev/null; then
 	status="$(termux-battery-status)"
 	percentage="$(echo "$status" | sed -n 's/^\s*"percentage": \(.*\),$/\1/p')"
-	is_charging="$([ "$(echo "$status" | sed -n 's/^\s*"status": "\(.*\)",$/\1/p')" != "DISCHARGING" ]; echo "$?")"
+	is_charging="$(
+		[ "$(echo "$status" | sed -n 's/^\s*"status": "\(.*\)",$/\1/p')" != "DISCHARGING" ]
+		echo "$?"
+	)"
 else
 	battery="$(find /sys/class/power_supply/BAT* | head -n1)"
 	if [ -z "$battery" ]; then
 		return
 	fi
 	percentage="$(cat "$battery/capacity")"
-	is_charging="$([ "$(cat "$battery/status")" != "Discharging" ]; echo "$?")"
+	is_charging="$(
+		[ "$(cat "$battery/status")" != "Discharging" ]
+		echo "$?"
+	)"
 fi
 
 if [ "$percentage" -gt 100 ]; then
@@ -23,5 +29,4 @@ else
 	icons=("󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹")
 fi
 
-index="$((percentage * (${#icons[@]} - 1) / 100))"
-echo "${icons[index]} $percentage% "
+echo "${icons[percentage * (${#icons[@]} - 1) / 100]} $percentage% "
