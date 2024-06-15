dd27e03df9c2fb19f12bd09cfae13e5e455cd72f
TarekSaeed0 <naminode2020@gmail.com>
2024-06-15 04:08:18 +0300

    .

--- .config/bash/fetch.sh
+++ .config/bash/fetch.sh
@@ -4,7 +4,7 @@ if command -v neofetch &>/dev/null; then
 	__fetch_id="$USER"
 	if ! [ -f "${TMPDIR:-/tmp}/$__fetch_id.fetch" ]; then
 		touch "${TMPDIR:-/tmp}/$__fetch_id.fetch"
-		trap "command rm \"\${TMPDIR:-/tmp}/\$__fetch_id.fetch\" &> /dev/null" EXIT
+		trap "command rm \"\${TMPDIR:-/tmp}/__fetch.\$__fetch_id\" &> /dev/null" EXIT
 		neofetch
 	fi
 fi
