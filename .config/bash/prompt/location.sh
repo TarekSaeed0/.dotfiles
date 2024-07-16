f280e9e0cee037fc35f8e0816fa5e495a757a8fc
TarekSaeed0 <naminode2020@gmail.com>
2024-07-16 17:53:18 +0300

    .

--- .config/bash/prompt/location.sh
+++ .config/bash/prompt/location.sh
@@ -26,16 +26,17 @@ __prompt_location() {
 	if [[ $path =~ ^(.*$separator)([^$separator]+)$ ]]; then
 		parent="${BASH_REMATCH[1]}"
 		name="${BASH_REMATCH[2]}"
+
+		if ((${#path} > maximum_length)); then
+			parent="${parent:$((${#parent} + ${#ellipsis} + ${#separator} + ${#name} - maximum_length))}"
+			if [[ $parent =~ [^$separator]*$separator?(.*)$ ]]; then
+				parent="${BASH_REMATCH[1]}"
+			fi
+			parent=" \001\e[1D$ellipsis\002$separator$parent"
+		fi
 	else
 		local parent=""
 		local name="$path"
-	fi
-	if ((${#path} > maximum_length)); then
-		parent="${parent:$((${#parent} + ${#ellipsis} + ${#separator} + ${#name} - maximum_length))}"
-		if [[ $parent =~ [^$separator]*$separator?(.*)$ ]]; then
-			parent="${BASH_REMATCH[1]}"
-		fi
-		parent=" \001\e[1D$ellipsis\002$separator$parent"
 	fi
 
 	echo -ne " \[\e[1D$icon\] $parent\[\e[0;1;48;2;24;24;37m\]$name "
