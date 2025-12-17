a55c0904d384d9e448a26edfff3ab22af49e589e
TarekSaeed0 <naminode2020@gmail.com>
2025-12-17 08:37:31 +0200

    .

--- .bashrc
+++ .bashrc
@@ -9,14 +9,29 @@ esac
 
 # Source bash components
 
+echo "" >"$HOME/bash.log"
 for component in multiplexer functions aliases misc prompt; do
 	if [ -r "$XDG_CONFIG_HOME/bash/$component.sh" ]; then
+		start_time=$(date +%s.%N)
 		. "$XDG_CONFIG_HOME/bash/$component.sh"
+		end_time=$(date +%s.%N)
+		elapsed_time=$(awk \
+			-v start="$start_time" \
+			-v end="$end_time" \
+			'BEGIN { print end - start }')
+		echo "$component took $elapsed_time seconds" >>"$HOME/bash.log"
 	fi
 done
 
 if [ -r "/usr/share/bash-completion/bash_completion" ]; then
 	if [ -r "$XDG_CONFIG_HOME/bash/completions.sh" ]; then
+		start_time=$(date +%s.%N)
 		. "$XDG_CONFIG_HOME/bash/completions.sh"
+		end_time=$(date +%s.%N)
+		elapsed_time=$(awk \
+			-v start="$start_time" \
+			-v end="$end_time" \
+			'BEGIN { print end - start }')
+		echo "bash_completion took $elapsed_time seconds" >>"$HOME/bash.log"
 	fi
 fi
--- .config/bash/completions.sh
+++ .config/bash/completions.sh
@@ -2,14 +2,35 @@
 
 for completion in dotfiles create_project; do
 	if [ -r "$XDG_CONFIG_HOME/bash/completions/${completion}.sh" ]; then
+		start_time=$(date +%s.%N)
 		. "$XDG_CONFIG_HOME/bash/completions/${completion}.sh"
+		end_time=$(date +%s.%N)
+		elapsed_time=$(awk \
+			-v start="$start_time" \
+			-v end="$end_time" \
+			'BEGIN { print end - start }')
+		echo "$completion completion took $elapsed_time seconds" >>"$HOME/bash.log"
 	fi
 done
 
 if command -v flutter &>/dev/null; then
+	start_time=$(date +%s.%N)
 	eval "$(flutter bash-completion)"
+	end_time=$(date +%s.%N)
+	elapsed_time=$(awk \
+		-v start="$start_time" \
+		-v end="$end_time" \
+		'BEGIN { print end - start }')
+	echo "flutter completion took $elapsed_time seconds" >>"$HOME/bash.log"
 fi
 
-if command -v ng &>/dev/null; then
-	source <(ng completion script)
-fi
+# if command -v ng &>/dev/null; then
+# 	start_time=$(date +%s.%N)
+# 	source <(ng completion script)
+# 	end_time=$(date +%s.%N)
+# 	elapsed_time=$(awk \
+# 		-v start="$start_time" \
+# 		-v end="$end_time" \
+# 		'BEGIN { print end - start }')
+# 	echo "ng completion took $elapsed_time seconds" >>"$HOME/bash.log"
+# fi
