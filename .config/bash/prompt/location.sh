f633f464ba2ad5d006d99e3d9cb5fb7921f6373b
TarekSaeed0 <naminode2020@gmail.com>
2026-03-25 23:08:41 +0200

    feat: improve performance of prompt by removing regex from location function

--- .config/bash/prompt/location.sh
+++ .config/bash/prompt/location.sh
@@ -27,20 +27,18 @@ __prompt_location() {
 	local ellipsis="…"
 
 	path="${path/#$HOME/\~}"
-	if [[ $path =~ ^(.*$separator)([^$separator]+)$ ]]; then
-		parent="${BASH_REMATCH[1]}"
-		name="${BASH_REMATCH[2]}"
+	local parent=""
+	local name="${path##*/}"
+	if [[ -n "$name" ]] && [[ "$path" == *"$separator"* ]]; then
+		parent="${path%"$separator"*}/"
 
 		if ((${#path} > maximum_length)); then
-			parent="${parent:$((${#parent} + ${#ellipsis} + ${#separator} + ${#name} - maximum_length))}"
-			if [[ $parent =~ [^$separator]*$separator?(.*)$ ]]; then
-				parent="${BASH_REMATCH[1]}"
-			fi
-			parent=" \001\e[1D$ellipsis\002$separator$parent"
+			local ellipsis="…"
+			parent="${parent: -((maximum_length - ${#name}))}"
+			parent="$ellipsis/${parent#*"$separator"}"
 		fi
 	else
-		local parent=""
-		local name="$path"
+		name="$path"
 	fi
 
 	echo -ne " \[\e[1D$icon\] $parent\[\e[0;1;48;2;24;24;37m\]$name "
