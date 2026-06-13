50df91f750b249a0f908796b59e611d3cea53bd4
Tarek Saeed <102486225+TarekSaeed0@users.noreply.github.com>
2026-06-13 10:17:49 +0300

    .

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 
-local function
-for function in cd ls cat diff retry dotfiles create_project; do
-	if [[ -r "$ZDOTDIR/functions/${function}.zsh" ]]; then
-		source "$ZDOTDIR/functions/${function}.zsh"
+for function_component in cd ls cat diff retry dotfiles create_project; do
+	if [[ -r "$ZDOTDIR/functions/${function_component}.zsh" ]]; then
+		source "$ZDOTDIR/functions/${function_component}.zsh"
 	fi
 done
